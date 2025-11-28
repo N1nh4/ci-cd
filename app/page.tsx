bf09d2d01d30ace047bfd39fa918c3b726a736a5
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-11-28 09:54:00 -0300

    hello world

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-9xl">Hello Word<h1>
+      <h1 className="text-9xl">Hello Word</h1>
     </div>
   );
 }
